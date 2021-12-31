ab9c03adaa7a619bda1663311a657a121f9da42c
Ryan Jarmon <rjarmon@users.noreply.github.com>
2021-12-30 20:28:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+Character Generator for D&D 5e
+================================================================
+A JavaScript console app that randomly generates a D&D character out of a pre-set collection of races, classes, backgrounds, and randomly rolled ability scores (strength, wisdom, dexterity, etc.)
+
+---------------------------------
+
+## Introduction
+Use this application to generate random playable or non-playable characters for your D&D campaign. Take the character as generated and put them in your game or use it as a source of inspiration to craft the perfect character for your setting or situation.
+
+## Table of Contents
+* [Features](#features)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Output](#output)
+* [License](#license)
+
+## Features
+Prints a randomly generated character (out of a pre-set collection of races, classes, backgrounds, and randomly rolled ability scores) to the console. Ability scores are randomly generated based off the house rule of rolling 4d6 7 times, rerolling 1's, adding the three highest numbers for each set, and dropping the lowest set
+
+## Technologies
+* JavaScript
+* Node.js
+
+## Setup
+
+To be able to run this script, [you need to setup Node.js locally in your machine.](https://www.codecademy.com/articles/setting-up-node-locally)
+
+## Usage
+Once you have Node.js set up, run the script.
+```
+...\your-script-directory> node script.js
+```
+
+## Output
+After you run the script, a character should be randomly generated and printed to the console. Below is an example. Yes they were rolled randomly. Yes I wish I had been rolling for an actual character.
+```
+...\your-script-directory> node script.js
+YOUR RANDOMLY GENERATED CHARACTER:
+Tortle Barbarian - Gladiator background
+Strength [10]
+Dexterity [13]
+Constitution [11]
+Intelligence [12]
+Wisdom [11]
+Charisma [15]
+```
+
+## License
+[MIT](/LICENSE.txt)
+
+---------
+
+[![Open in Visual Studio Code](https://open.vscode.dev/badges/open-in-vscode.svg)](https://open.vscode.dev/rjarmon/charactergenerator)
+![GitHub contributors](https://img.shields.io/github/contributors/rjarmon/charactergenerator?style=plastic)
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/rjarmon/charactergenerator?style=plastic)
+![GitHub forks](https://img.shields.io/github/forks/rjarmon/charactergenerator?style=plastic)
+![GitHub stars](https://img.shields.io/github/stars/rjarmon/charactergenerator?style=plastic)
+![GitHub](https://img.shields.io/github/license/rjarmon/charactergenerator)
+![GitHub issues](https://img.shields.io/github/issues/rjarmon/charactergenerator?style=plastic)
+![Discord](https://img.shields.io/discord/926195512644874271?color=blue&label=Discord&logo=Discord&style=plastic)
+![Twitch Status](https://img.shields.io/twitch/status/thepolkawarlord?style=social)
+![YouTube Channel Subscribers](https://img.shields.io/youtube/channel/subscribers/UCAtxepM8_SW6epHIVGuWdLg?style=social)
---
+++ script.js
@@ -0,0 +1,50 @@
+const characterRaces = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tiefling", "Orc of Exandria", "Leonin", "Satyr", "Fairy", "Harengon", "Owlin", "Aarakocra", "Genasi", "Goliath", "Aasimar", "Bugbear", "Firbolg", "Goblin", "Hobgoblin", "Kenku", "Kobold", "Lizardfolk", "Orc", "Tabaxi", "Triton", "Yuan-ti Pureblood", "Feral Tiefling", "Tortle", "Changeling", "Kalashtar", "Orc of Eberron", "Shifter", "Warforged", "Gith", "Centaur", "Loxodon", "Minotaur", "Simic Hybrid", "Vedalken", "Verdan", "Locathah", "Grung"];
+
+const characterClasses = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer", "Blood Hunter"];
+
+const characterBackgrounds = ["Acolyte", "Acolyte - Baldur's Gate", "Acolyte (Luxonborn)", "Anthropologist", "Archaeologist", "Athlete", "Azorius", "Functionary", "Boros Legionnaire", "Celebrity Adventurer’s Scion", "Charlatan", "Charlatan - Baldur’s Gate", "City Watch / Investigator", "Clan Crafter", "Cloistered Scholar", "Courtier", "Criminal - Baldur’s Gate", "Criminal (Myriad Operative)", "Criminal / Spy", "Dimir Operative", "Entertainer", "Entertainer - Baldur’s Gate", "Faceless", "Faction Agent", "Failed Merchant", "Far Traveler", "Feylost", "Fisher", "Folk Hero", "Folk Hero - Baldur’s Gate", "Gambler", "Gladiator", "Golgari Agent", "Grinner", "Gruul Anarch", "Guild Artisan - Baldur’s Gate", "Guild Artisan / Guild Merchant", "Haunted One", "Hermit", "Hermit - Baldur’s Gate", "House Agent (Cannith)", "House Agent (Deneith)", "House Agent (Ghallanda)", "House Agent (Jorasco)", "House Agent (Kundarak)", "House Agent (Lyrandar)", "House Agent (Medani)", "House Agent (Orien)", "House Agent (Orien)", "House Agent (Orien)", "House Agent (Tharashk)", "House Agent (Thuranni)", "House Agent (Vadalis)", "Inheritor", "Investigator", "Izzet Engineer", "Knight", "Knight of the Order", "Lorehold Student", "Marine", "Mercenary Veteran", "Noble", "Noble - Baldur’s Gate", "Orzhov Representative", "Outlander", "Outlander - Baldur’s Gate", "Pirate", "Plaintiff", "Prismari Student", "Quandrix Student", "Rakdos Cultist", "Rival Intern", "Sage", "Sage - Baldur’s Gate", "Sage (Cobalt Scholar)", "Sailor", "Sailor - Baldur’s Gate", "Sailor (Revelry Pirate)", "Selesnya Initiate", "Shipwright", "Silverquill Student", "Simic Scientist", "Smuggler", "Soldier", "Soldier - Baldur’s Gate", "Spy (Augen Trust)", "Urban Bounty Hunter", "Urchin", "Urchin - Baldur’s Gate", "Uthgardt Tribe Member", "Volstrucker Agent", "Waterdhavian Noble", "Witchlight Hand", "Witherbloom Student"];
+
+rollD6 = () => Math.floor(Math.random() * (6 - 1) + 2);
+
+rollAbilityScores = () => {
+    var diceRolls = [];
+
+    let firstD6 = rollD6();
+    diceRolls.push(firstD6);
+
+    let secondD6 = rollD6();
+    diceRolls.push(secondD6);    
+
+    let thirdD6 = rollD6();
+    diceRolls.push(thirdD6);
+
+    let fourthD6 = rollD6();
+    diceRolls.push(fourthD6);
+
+    function dropLowest(rolls) {
+        rolls = rolls.slice();
+        rolls.splice(rolls.indexOf(Math.min.apply(null, rolls)),1);
+        return rolls;
+    };
+
+    const abilityScore = dropLowest(diceRolls).reduce((a,b) => a + b, 0);
+    return abilityScore;
+}
+
+const getRace = Math.floor(Math.random() * characterRaces.length);
+const characterRace = characterRaces[getRace];
+
+const getClass = Math.floor(Math.random() * characterClasses.length);
+const characterClass = characterClasses[getClass];
+
+const getBackground = Math.floor(Math.random() * characterBackgrounds.length);
+const characterBackground = characterBackgrounds[getBackground]
+
+const strengthScore = rollAbilityScores();
+const dexterityScore = rollAbilityScores();
+const constitutionScore = rollAbilityScores();
+const intelligenceScore = rollAbilityScores();
+const wisdomScore = rollAbilityScores();
+const charismaScore = rollAbilityScores();
+
+console.log(`YOUR RANDOMLY GENERATED CHARACTER:\n${characterRace} ${characterClass} - ${characterBackground} background\nStrength [${strengthScore}]\nDexterity [${dexterityScore}]\nConstitution [${constitutionScore}]\nIntelligence [${intelligenceScore}]\nWisdom [${wisdomScore}]\nCharisma [${charismaScore}]`)
